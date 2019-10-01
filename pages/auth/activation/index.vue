be45bab18cd346c4e288ba60a35d5bfeb0aa6df0
Renz Mergenio <zner.mergenio@gmail.com>
2019-10-01 15:24:06 +0800

    Change grid in registration form

--- pages/auth/activation/index.vue
+++ pages/auth/activation/index.vue
@@ -1,17 +1,19 @@
 <template>
-    <v-flex xs12 lg5>
-        <v-alert
-            prominent
-            type="error"
-        >
-            <v-row align="center">
-                <v-col class="grow">Please wait for the administrator to activate your account.</v-col>
-                <v-col class="shrink">
-                    <v-btn small @click="$auth.logout()">Logout</v-btn>
-                </v-col>
-            </v-row>
-        </v-alert>
-    </v-flex>
+    <v-row>
+        <v-col cols="12">
+            <v-alert
+                prominent
+                type="error"
+            >
+                <v-row align="center">
+                    <v-col class="grow">Please wait for the administrator to activate your account.</v-col>
+                    <v-col class="shrink">
+                        <v-btn small @click="$auth.logout()">Logout</v-btn>
+                    </v-col>
+                </v-row>
+            </v-alert>
+        </v-col>
+    </v-row>
 </template>
 
 <script>
--- pages/auth/form/index.vue
+++ pages/auth/form/index.vue
@@ -1,159 +1,161 @@
 <template>
-    <v-flex xs12 lg7>
-        <v-card>
-            <v-card-title>
-                <h3>Registration Form</h3>
-            </v-card-title>
-            <v-divider></v-divider>
-            <v-container grid-list-lg>
-                <v-layout row wrap>
-                    <v-flex xs12 lg4>
-                        <v-text-field
-                            v-model="form.first_name"
-                            label="First Name"
-                            placeholder="Juan"
-                        ></v-text-field>
-                    </v-flex>
-                    <v-flex xs12 lg4>
-                        <v-text-field
-                            v-model="form.middle_name"
-                            label="Middle Name"
-                            placeholder="Juan"
-                        ></v-text-field>
-                    </v-flex>
-                    <v-flex xs12 lg4>
-                        <v-text-field
-                            v-model="form.last_name"
-                            label="Last Name"
-                            placeholder="Dela Cruz"
-                        ></v-text-field>
-                    </v-flex>
-                    <v-flex xs12 lg6>
-                        <DatePicker 
-                            label="Birthdate"
-                            placeholder="2000-01-01"
-                            v-model="form.birth_date"
-                        ></DatePicker>
-                    </v-flex>
-                    <v-flex xs12 lg6>
-                        <v-select
-                            :items="['Single', 'Married']"
-                            label="Civil Status"
-                            v-model="form.civil_status"
-                        ></v-select>
-                    </v-flex>
-                    <v-flex xs12 lg6>
-                        <v-text-field
-                            v-model="form.present_address"
-                            label="Present Address"
-                            placeholder="Address"
-                        ></v-text-field>
-                    </v-flex>
-                    <v-flex xs12 lg6>
-                        <v-text-field
-                            v-model="form.permanent_address"
-                            label="Permanent Address"
-                            placeholder="Address"
-                        ></v-text-field>
-                    </v-flex>
-                    <v-flex xs12 lg6>
-                        <v-text-field
-                            v-model="form.contact_no_1"
-                            label="Contact No. #1"
-                            placeholder="09123456789"
-                        ></v-text-field>
-                    </v-flex>
-                    <v-flex xs12 lg6>
-                        <v-text-field
-                            v-model="form.contact_no_2"
-                            label="Contact No. #2"
-                            placeholder="09123456789"
-                        ></v-text-field>
-                    </v-flex>
-                    <v-flex xs12 lg3>
-                        <v-text-field
-                            v-model="form.tin"
-                            label="TIN"
-                            placeholder="000-000-000-0"
-                        ></v-text-field>
-                    </v-flex>
-                    <v-flex xs12 lg3>
-                        <v-text-field
-                            v-model="form.sss"
-                            label="SSS"
-                            placeholder="000-000-000-0"
-                        ></v-text-field>
-                    </v-flex>
-                    <v-flex xs12 lg3>
-                        <v-text-field
-                            v-model="form.pagibig"
-                            label="PAGIBIG"
-                            placeholder="000-000-000-0"
-                        ></v-text-field>
-                    </v-flex>
-                    <v-flex xs12 lg3>
-                        <v-text-field
-                            v-model="form.philhealth"
-                            label="PhilHealth"
-                            placeholder="000-000-000-0"
-                        ></v-text-field>
-                    </v-flex>
-                    <v-flex xs12 lg4>
-                        <v-text-field
-                            v-model="form.employee_id"
-                            label="Employee ID"
-                            placeholder="20001234"
-                        ></v-text-field>
-                    </v-flex>
-                    <v-flex xs12 lg4>
-                        <v-select
-                            :items="branches"
-                            item-text="display_name"
-                            item-value="id"
-                            label="Branch"
-                            placeholder="Select Branch"
-                            v-model="form.branch_id"
-                        ></v-select>
-                    </v-flex>
-                    <v-flex xs12 lg4>
-                        <DatePicker
-                            label="Date Hired"
-                            placeholder="2000-01-01"
-                            v-model="form.date_hired"
-                        ></DatePicker>
-                    </v-flex>
-                    <v-flex xs12 lg6>
-                        <v-select
-                            :items="departments"
-                            item-text="display_name"
-                            item-value="id"
-                            label="Department"
-                            placeholder="Select department"
-                            v-model="form.department_id"
-                        ></v-select>
-                    </v-flex>
-                    <v-flex xs12 lg6>
-                        <v-text-field
-                            v-model="form.position"
-                            label="Position"
-                            placeholder="IT Specialist"
-                        ></v-text-field>
-                    </v-flex>
-                    <v-flex xs12>
-                        <v-text-field
-                            v-model="form.skype_id"
-                            label="Skype"
-                            placeholder="live:test_1"
-                        ></v-text-field>
-                    </v-flex>
-                </v-layout>
-            </v-container>
-            <v-divider></v-divider>
-            <v-card-actions>
-                <v-btn ref="submit" @click="submit" block color="info" dark>Submit</v-btn>
-            </v-card-actions>
-        </v-card>
-    </v-flex>
+    <v-row justify="center" align="center">
+        <v-col cols="12" xs="12" sm="6" md="6" lg="5">
+            <v-card>
+                <v-card-title>
+                    <h3>Registration Form</h3>
+                </v-card-title>
+                <v-divider></v-divider>
+                <v-container grid-list-lg>
+                    <v-row dense>
+                        <v-col cols="12" xs="12" md="4" lg="4">
+                            <v-text-field
+                                v-model="form.first_name"
+                                label="First Name"
+                                placeholder="Juan"
+                            ></v-text-field>
+                        </v-col>
+                        <v-col cols="12" xs="12" md="4" lg="4">
+                            <v-text-field
+                                v-model="form.middle_name"
+                                label="Middle Name"
+                                placeholder="Juan"
+                            ></v-text-field>
+                        </v-col>
+                        <v-col cols="12" xs="12" md="4" lg="4">
+                            <v-text-field
+                                v-model="form.last_name"
+                                label="Last Name"
+                                placeholder="Dela Cruz"
+                            ></v-text-field>
+                        </v-col>
+                        <v-col cols="12" xs="12" md="6" lg="6">
+                            <DatePicker 
+                                label="Birthdate"
+                                placeholder="2000-01-01"
+                                v-model="form.birth_date"
+                            ></DatePicker>
+                        </v-col>
+                        <v-col cols="12" xs="12" md="6" lg="6">
+                            <v-select
+                                :items="['Single', 'Married']"
+                                label="Civil Status"
+                                v-model="form.civil_status"
+                            ></v-select>
+                        </v-col>
+                        <v-col cols="12" xs="12" md="6" lg="6">
+                            <v-text-field
+                                v-model="form.present_address"
+                                label="Present Address"
+                                placeholder="Address"
+                            ></v-text-field>
+                        </v-col>
+                        <v-col cols="12" xs="12" md="6" lg="6">
+                            <v-text-field
+                                v-model="form.permanent_address"
+                                label="Permanent Address"
+                                placeholder="Address"
+                            ></v-text-field>
+                        </v-col>
+                        <v-col cols="12" xs="12" md="6" lg="6">
+                            <v-text-field
+                                v-model="form.contact_no_1"
+                                label="Contact No. #1"
+                                placeholder="09123456789"
+                            ></v-text-field>
+                        </v-col>
+                        <v-col cols="12" xs="12" md="6" lg="6">
+                            <v-text-field
+                                v-model="form.contact_no_2"
+                                label="Contact No. #2"
+                                placeholder="09123456789"
+                            ></v-text-field>
+                        </v-col>
+                        <v-col cols="12" xs="12" md="3" lg="3">
+                            <v-text-field
+                                v-model="form.tin"
+                                label="TIN"
+                                placeholder="000-000-000-0"
+                            ></v-text-field>
+                        </v-col>
+                        <v-col cols="12" xs="12" md="3" lg="3">
+                            <v-text-field
+                                v-model="form.sss"
+                                label="SSS"
+                                placeholder="000-000-000-0"
+                            ></v-text-field>
+                        </v-col>
+                        <v-col cols="12" xs="12" md="3" lg="3">
+                            <v-text-field
+                                v-model="form.pagibig"
+                                label="PAGIBIG"
+                                placeholder="000-000-000-0"
+                            ></v-text-field>
+                        </v-col>
+                        <v-col cols="12" xs="12" md="3" lg="3">
+                            <v-text-field
+                                v-model="form.philhealth"
+                                label="PhilHealth"
+                                placeholder="000-000-000-0"
+                            ></v-text-field>
+                        </v-col>
+                        <v-col cols="12" xs="12" sm="4" md="4" lg="4">
+                            <v-text-field
+                                v-model="form.employee_id"
+                                label="Employee ID"
+                                placeholder="20001234"
+                            ></v-text-field>
+                        </v-col>
+                        <v-col cols="12" xs="12" sm="4" md="4" lg="4">
+                            <v-select
+                                :items="branches"
+                                item-text="display_name"
+                                item-value="id"
+                                label="Branch"
+                                placeholder="Select Branch"
+                                v-model="form.branch_id"
+                            ></v-select>
+                        </v-col>
+                        <v-col cols="12" sm="4" md="4" lg="4">
+                            <DatePicker
+                                label="Date Hired"
+                                placeholder="2000-01-01"
+                                v-model="form.date_hired"
+                            ></DatePicker>
+                        </v-col>
+                        <v-col cols="12" sm="12" md="6" lg="6">
+                            <v-select
+                                :items="departments"
+                                item-text="display_name"
+                                item-value="id"
+                                label="Department"
+                                placeholder="Select department"
+                                v-model="form.department_id"
+                            ></v-select>
+                        </v-col>
+                        <v-col cols="12" sm="12" md="6" lg="6">
+                            <v-text-field
+                                v-model="form.position"
+                                label="Position"
+                                placeholder="IT Specialist"
+                            ></v-text-field>
+                        </v-col>
+                        <v-col cols="12">
+                            <v-text-field
+                                v-model="form.skype_id"
+                                label="Skype"
+                                placeholder="live:test_1"
+                            ></v-text-field>
+                        </v-col>
+                    </v-row>
+                </v-container>
+                <v-divider></v-divider>
+                <v-card-actions>
+                    <v-btn ref="submit" @click="submit" block color="info" dark>Submit</v-btn>
+                </v-card-actions>
+            </v-card>
+        </v-col>
+    </v-row>
 </template>
 
 <script>
