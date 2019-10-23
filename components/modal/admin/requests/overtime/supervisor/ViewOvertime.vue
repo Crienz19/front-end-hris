cc458ee19a9548fcc1d338f3f347132bf596c32a
Renz Mergenio <zner.mergenio@gmail.com>
2019-10-23 15:44:58 +0800

    Admin Role - OT Supervisor Approve Button

--- components/modal/admin/requests/overtime/supervisor/ViewOvertime.vue
+++ components/modal/admin/requests/overtime/supervisor/ViewOvertime.vue
@@ -45,7 +45,7 @@
           </v-col>
         </v-row>
       </v-container>
-      <!--<v-card-actions>
+      <v-card-actions>
           <v-btn :disabled="overtime.status == 'Approved'" color="success" block @click="approveOvertime">Approve</v-btn>
           <v-btn :disabled="overtime.status == 'Disapproved'" color="error" block @click="disapproveOvertime">Disapprove</v-btn>
       </v-card-actions>-->
